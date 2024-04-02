15c67ab64ef88c92deaf07c35a9dfdc279c822fe
OscarCordobesNavarro <alu0101478081@ull.edu.es>
2024-04-02 23:49:39 +0000

    ejemplos para la modificacion

--- src/cliente.ts
+++ src/cliente.ts
@@ -47,13 +47,110 @@
 //   console.log(`File was ${prev.size} bytes before it was modified.`);
 //   console.log(`Now file is ${curr.size} bytes.`);
 
-//   // Imprimimos la carpeta actual
-//     const pwd = spawn("pwd");
-//     pwd.stdout.pipe(process.stdout);
 //   // Creamos un proceso hijo que ejecuta el comando cat
-//   const cat = spawn("cat ../archivo.txt");
+//   const cat = spawn("cat", ["archivo.txt"]);
 //   cat.stdout.pipe(process.stdout);
 // });
 
 // Primera toma de contacto con event emitter
+/**
+ * Lo que estamos haciendo aquí es generar un proceso que ejecuta un comando, lo dejamos en segundo plano
+ * y va emitiendo ciertos eventos, en este caso emite eventos de tipo data, que son los datos que se van
+ * leyendo del proceso hijo. Y cuando el proceso hijo termina, emite un evento de tipo close, donde
+ * ya podemos confirmar que el proceso ya ha terminado y se ha caturado toda la salida.
+ */
 
+// import {watchFile} from 'fs';
+// import {spawn} from 'child_process';
+
+// watchFile('archivo.txt', (curr, prev) => {
+//   console.log(`File was ${prev.size} bytes before it was modified.`);
+//   console.log(`Now file is ${curr.size} bytes.`);
+
+//   const wc = spawn('wc', ['archivo.txt']);
+
+//   let wcOutput = '';
+//   wc.stdout.on('data', (piece) => wcOutput += piece);
+
+//   wc.on('close', () => {
+//     const wcOutputAsArray = wcOutput.split(/\s+/);
+//     console.log(`File archivo.txt has ${wcOutputAsArray[1]} lines`);
+//     console.log(`File archivo.txt has ${wcOutputAsArray[2]} words`);
+//     console.log(`File archivo.txt has ${wcOutputAsArray[3]} characters`);
+//     console.log(wcOutputAsArray[4]);
+//   });
+// });
+
+// EJEMPLO DE LECTURA Y ESCRITURA ASINCRONA DE FICHEROS -> CREACION DE STREAMS DE LECTURA Y ESCRITURA
+/**
+ * Aquí lo que estoy haciendo es crear un ReadStream, que es un stream de lectura.
+ *
+ * Este stream de lectura se encarga de leer el archivo archivo.txt y emitir eventos de tipo data
+ * y lo que lo hace especial es que va poco a poco leyendo el archivo, en trozos.
+ */
+// import {createReadStream} from 'fs';
+
+// const inputStream = createReadStream('archivo.txt');
+
+// inputStream.on('data', (piece) => {
+//   process.stdout.write(piece);
+// });
+
+// inputStream.on('error', (err) => {
+//   process.stderr.write(err.message);
+// });
+
+// --------------------------------------------- EJEMPLO DE SOCKETS ---------------------------------------------
+
+/**
+ * Y ahora lo que  estamos haciendo es parsear esa informacion en un JSON real
+ */
+
+// import net from "net";
+
+// const client = net.connect({ port: 60300 });
+
+// client.on("data", (dataJSON) => {
+//   const message = JSON.parse(dataJSON.toString());
+
+//   if (message.type === "watch") {
+//     console.log(`Connection established: watching file ${message.file}`);
+//   } else if (message.type === "change") {
+//     console.log("File has been modified.");
+//     console.log(`Previous size: ${message.prevSize}`);
+//     console.log(`Current size: ${message.currSize}`);
+//   } else {
+//     console.log(`Message type ${message.type} is not valid`);
+//   }
+// });
+
+/**
+ * Código mejorado para gestionar que los mensajes lleguen incompletos
+ * 
+ * Como por ejemplo si creamos un stream, puede ser que el mensaje llegue incompleto
+ * 
+ * Y lo que hacemos es ir guardanto la informacion en los eventos de tipo data
+ * Y cuando se obtenga un evento de tipo end, es cuando se procesa toda la informacion
+ */
+import net from 'net';
+
+const client = net.connect({port: 60300});
+
+let wholeData = '';
+client.on('data', (dataChunk) => {
+  wholeData += dataChunk;
+});
+
+client.on('end', () => {
+  const message = JSON.parse(wholeData);
+
+  if (message.type === 'watch') {
+    console.log(`Connection established: watching file ${message.file}`);
+  } else if (message.type === 'change') {
+    console.log('File has been modified.');
+    console.log(`Previous size: ${message.prevSize}`);
+    console.log(`Current size: ${message.currSize}`);
+  } else {
+    console.log(`Message type ${message.type} is not valid`);
+  }
+});
--- src/servidor.ts
+++ src/servidor.ts
@@ -8,3 +8,130 @@
  * Correo: alu0101478081@ull.edu.es
  * Fecha: 02/04/2024
  */
+
+// --------------------------------------------- EJEMPLO DE SOCKETS ---------------------------------------------
+
+
+/**
+ * Lo que estamos haciendo aquí es crear un servidor
+ * Y cuando se produzca una conexión, se envía un mensaje al cliente
+ * con la funcion connection.write()
+ * 
+ * Y Cuando se produce una desconexión, se muestra un mensaje por consola en el servidor
+ */
+// import net from 'net';
+
+// net.createServer((connection) => {
+//   console.log('A client has connected.');
+
+//   connection.write(`Connection established.`);
+
+//   connection.on('close', () => {
+//     console.log('A client has disconnected.');
+//   });
+// }).listen(60300, () => {
+//   console.log('Waiting for clients to connect.');
+// });
+
+// OTRO EJEMPLO MAS COMPLEJO
+// import net from 'net';
+// import {watchFile} from 'fs';
+
+/**
+ * Lo que estamos haciendo en este código es que vamos a crear el servidor
+ * "apuntando" a un archivo que será visualizado constantemente con el watchfile
+ * todos los cambios se los pasaremos al cliente
+ */
+// if (process.argv.length !== 3) {
+//   console.log('Please, provide a filename.');
+// } else {
+//   const fileName = process.argv[2];
+
+//   net.createServer((connection) => {
+//     console.log('A client has connected.');
+
+//     connection.write(`Connection established: watching file ${fileName}.\n`);
+
+//     watchFile(fileName, (curr, prev) => {
+//       connection.write(`Size of file ${fileName} was ${prev.size}.\n`);
+//       connection.write(`Size of file ${fileName} now is ${curr.size}.\n`);
+//     });
+
+//     connection.on('close', () => {
+//       console.log('A client has disconnected.');
+//     });
+//   }).listen(60300, () => {
+//     console.log('Waiting for clients to connect.');
+//   });
+// }
+
+// AHORA CON JSON's
+// import net from 'net';
+// import {watchFile} from 'fs';
+/**
+ * Lo que estamos haciendo es crear un servidor otra vez apuntado al archvio que le
+ * pasamos por parámetros al ejecutar el servidor. Lo que hacemos es pasarle un JSON al cliente
+ * 
+ * Y nos ponemos a observar al archivo, cada vez que cambie lo que hacemos es enviar un JSON en vez de 
+ * una cadena de texto
+ */
+
+// if (process.argv.length !== 3) {
+//   console.log('Please, provide a filename.');
+// } else {
+//   const fileName = process.argv[2];
+
+//   net.createServer((connection) => {
+//     console.log('A client has connected.');
+
+//     connection.write(JSON.stringify({'type': 'watch', 'file': fileName}) +
+//       '\n');
+
+//     watchFile(fileName, (curr, prev) => {
+//       connection.write(JSON.stringify({
+//         'type': 'change', 'prevSize': prev.size, 'currSize': curr.size}) +
+//          '\n');
+//     });
+
+//     connection.on('close', () => {
+//       console.log('A client has disconnected.');
+//     });
+//   }).listen(60300, () => {
+//     console.log('Waiting for clients to connect.');
+//   });
+// }
+
+
+// VERSION PARA ENVIAR DOS DATOS SEPARADOS
+/**
+ * Lo que estamos es enviando los paquetes y cuando se envía el segundo paquete
+ * se envia un mensaje de desconexión y se cierra la conexión
+ */
+
+import net from 'net';
+
+const server = net.createServer((connection) => {
+  console.log('A client has connected.');
+
+  const firstData = '{"type": "change", "prevSize": 13';
+  const secondData = ', "currSize": 27}\n';
+
+  connection.write(firstData);
+
+  const timer = setTimeout(() => {
+    connection.write(secondData);
+    connection.end();
+  }, 500);
+
+  connection.on('end', () => {
+    clearTimeout(timer);
+  });
+
+  connection.on('close', () => {
+    console.log('A client has disconnected');
+  });
+});
+
+server.listen(60300, () => {
+  console.log('Waiting for clients to connect.');
+});
